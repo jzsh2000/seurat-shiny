3f5fc4bbe288749ec49a610bf1830cee60552d9f
Xiaoyang Jin <jinxy1991@gmail.com>
2018-06-22 16:19:40 +0800

    Use navbarPage and include a new tabPanel

--- shiny/ui.R
+++ shiny/ui.R
@@ -18,6 +18,7 @@ library(glue)
 resource_list <- read_csv('data/resource_list.csv',
                           col_types = 'ccccd')
 
+# Read configure parameters
 if (file.exists('config.txt')) {
     source('config.txt')
 }
@@ -26,128 +27,143 @@ if (!exists('panel_name_1')) panel_name_1 = 'Gene expression value'
 if (!exists('panel_name_2')) panel_name_2 = 'Gene co-expression'
 if (!exists('panel_name_3')) panel_name_3 = 'Cluster signature genes'
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(theme = shinytheme('cerulean'),
+# Define UI
+shinyUI(navbarPage(
 
-  useShinyjs(),
-  tags$head(tags$link(rel = "shortcut icon", href = "dc.ico"),
-            tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
-            ),
+  theme = shinytheme('cerulean'),
+  title = app_title,
+  id = 'dc_sc',
+  header = tags$head(tags$link(rel = "shortcut icon",
+                               href = "dc.ico"),
+                     tags$link(rel = "stylesheet",
+                               type = "text/css",
+                               href = "style.css")),
+  selected = 'datasets',
 
-  # Application title
-  titlePanel(app_title),
+  tabPanel(
+      tags$b('About'), value = 'about',
+      column(1),
+      column(
+          width = 10,
+          wellPanel(includeMarkdown("www/About.Rmd"))
+      ),
+      column(1)
+  ),
 
-  hr(),
+  tabPanel(
+      tags$b('Single-cell Datasets'),
+      value = 'datasets',
+      useShinyjs(),
 
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-        selectizeInput(inputId = 'dataset',
-                       label = 'Dataset [*]',
-                       choices = c(' ' = 'none',
-                                   deframe(resource_list[,c('description',
-                                                            'label')]))),
-        hidden(
-            tags$div(
-                id = 'dat_config',
-                sliderInput(inputId = 'resolution',
-                            label = 'Cluster resolution',
-                            min = 0.1, max = 1.5, value = 0.8,
-                            step = 0.1),
-                hr(),
-                conditionalPanel(
-                    glue('input.tabset_main == "{panel_name_1}"'),
-                    # checkboxInput(inputId = 'cb_label',
-                    #               label = 'Label cluster center',
-                    #               value = TRUE),
-                    #
-                    checkboxInput(inputId = 'cb_cellranger',
-                                  label = 'Use original t-SNE',
-                                  value = FALSE),
-                    checkboxInput(inputId = 'cb_allpt',
-                                  label = 'Show all cells',
-                                  value = FALSE),
-                    checkboxInput(inputId = 'cb_showsize',
-                                  label = 'Show cluster size',
-                                  value = FALSE),
-                    checkboxInput(inputId = 'cb_subset',
-                                  label = 'Use subset cluster',
-                                  value = FALSE),
-                    textInput(inputId = 'tx_gene',
-                              label = 'Gene name',
-                              placeholder = 'Your awesome gene'),
-                    hr(),
-                    selectizeInput(inputId = 'cluster_id_subset',
-                                   label = 'Use cluster',
-                                   choices = NULL,
-                                   selected = NULL,
-                                   multiple = TRUE
-                    ),
-                    sliderInput(inputId = 'resolution_subset',
-                                label = 'Cluster resolution',
-                                min = 0.1, max = 1.5, value = 0.8,
-                                step = 0.1)
-                ),
-                conditionalPanel(
-                    glue('input.tabset_main == "{panel_name_2}"'),
-                    selectizeInput(inputId = 'cluster_id',
-                                   label = 'Use cluster',
-                                   choices = NULL,
-                                   selected = NULL,
-                                   multiple = TRUE
-                    ),
-                    textInput(inputId = 'tx_gene1',
-                              label = 'Gene name #1 [*]',
-                              placeholder = 'Your awesome gene'),
-                    textInput(inputId = 'tx_gene2',
-                              label = 'Gene name #2 [*]',
-                              placeholder = 'Your awesome gene')
-                ),
-                conditionalPanel(
-                    glue('input.tabset_main == "{panel_name_3}"'),
-                    selectizeInput(inputId = 'sig_cluster_1',
-                                   label = 'Use cluster [*]',
-                                   choices = '',
-                                   multiple = FALSE),
-                    selectizeInput(inputId = 'sig_cluster_2',
-                                   label = 'Compare to',
-                                   choices = '(All other cells)',
-                                   # selected = NULL,
-                                   multiple = FALSE),
-                    radioButtons(inputId = 'marker_pos',
-                                 label = 'Find markers',
-                                 inline = TRUE,
-                                 choices = c('positive' = 'pos',
-                                             'negative' = 'neg'))
-            )
-        )
+      sidebarLayout(
+          sidebarPanel(
+              selectizeInput(inputId = 'dataset',
+                             label = 'Dataset [*]',
+                             choices = c(' ' = 'none',
+                                         deframe(resource_list[,c('description',
+                                                                  'label')]))),
+              hidden(
+                  tags$div(
+                      id = 'dat_config',
+                      sliderInput(inputId = 'resolution',
+                                  label = 'Cluster resolution',
+                                  min = 0.1, max = 1.5, value = 0.8,
+                                  step = 0.1),
+                      hr(),
+                      conditionalPanel(
+                          glue('input.tabset_main == "{panel_name_1}"'),
+                          # checkboxInput(inputId = 'cb_label',
+                          #               label = 'Label cluster center',
+                          #               value = TRUE),
+                          #
+                          checkboxInput(inputId = 'cb_cellranger',
+                                        label = 'Use original t-SNE',
+                                        value = FALSE),
+                          checkboxInput(inputId = 'cb_allpt',
+                                        label = 'Show all cells',
+                                        value = FALSE),
+                          checkboxInput(inputId = 'cb_showsize',
+                                        label = 'Show cluster size',
+                                        value = FALSE),
+                          checkboxInput(inputId = 'cb_subset',
+                                        label = 'Use subset cluster',
+                                        value = FALSE),
+                          textInput(inputId = 'tx_gene',
+                                    label = 'Gene name',
+                                    placeholder = 'Your awesome gene'),
+                          hr(),
+                          selectizeInput(inputId = 'cluster_id_subset',
+                                         label = 'Use cluster',
+                                         choices = NULL,
+                                         selected = NULL,
+                                         multiple = TRUE
+                          ),
+                          sliderInput(inputId = 'resolution_subset',
+                                      label = 'Cluster resolution',
+                                      min = 0.1, max = 1.5, value = 0.8,
+                                      step = 0.1)
+                      ),
+                      conditionalPanel(
+                          glue('input.tabset_main == "{panel_name_2}"'),
+                          selectizeInput(inputId = 'cluster_id',
+                                         label = 'Use cluster',
+                                         choices = NULL,
+                                         selected = NULL,
+                                         multiple = TRUE
+                          ),
+                          textInput(inputId = 'tx_gene1',
+                                    label = 'Gene name #1 [*]',
+                                    placeholder = 'Your awesome gene'),
+                          textInput(inputId = 'tx_gene2',
+                                    label = 'Gene name #2 [*]',
+                                    placeholder = 'Your awesome gene')
+                      ),
+                      conditionalPanel(
+                          glue('input.tabset_main == "{panel_name_3}"'),
+                          selectizeInput(inputId = 'sig_cluster_1',
+                                         label = 'Use cluster [*]',
+                                         choices = '',
+                                         multiple = FALSE),
+                          selectizeInput(inputId = 'sig_cluster_2',
+                                         label = 'Compare to',
+                                         choices = '(All other cells)',
+                                         # selected = NULL,
+                                         multiple = FALSE),
+                          radioButtons(inputId = 'marker_pos',
+                                       label = 'Find markers',
+                                       inline = TRUE,
+                                       choices = c('positive' = 'pos',
+                                                   'negative' = 'neg'))
+                      )
+                  )
 
-        )
-    ),
+              )
+          ),
 
-    # Show a plot of the generated distribution
-    mainPanel(
-        hidden(
-            tags$div(
-                id = 'dat_panel',
-                tabsetPanel(id = 'tabset_main', type = 'tabs',
-                            tabPanel(
-                                title = panel_name_1,
-                                plotOutput('plot_gene_expr')
-                            ),
-                            tabPanel(
-                                title = panel_name_2,
-                                # verbatimTextOutput('coefficient'),
-                                plotOutput('plot_gene_expr2')
-                            ),
-                            tabPanel(
-                                title = panel_name_3,
-                                tags$p(id = 'warning_info'),
-                                DT::dataTableOutput('table_sig_gene')
-                            )
-                )
-            )
-        )
-    )
+          # Show a plot of the generated distribution
+          mainPanel(
+              hidden(
+                  tags$div(
+                      id = 'dat_panel',
+                      tabsetPanel(id = 'tabset_main', type = 'tabs',
+                                  tabPanel(
+                                      title = panel_name_1,
+                                      plotOutput('plot_gene_expr')
+                                  ),
+                                  tabPanel(
+                                      title = panel_name_3,
+                                      tags$p(id = 'warning_info'),
+                                      DT::dataTableOutput('table_sig_gene')
+                                  ),
+                                  tabPanel(
+                                      title = panel_name_2,
+                                      # verbatimTextOutput('coefficient'),
+                                      plotOutput('plot_gene_expr2')
+                                  )
+                      )
+                  )
+              )
+          )
+      )
   )
 ))
